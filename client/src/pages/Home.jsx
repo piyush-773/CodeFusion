b289cf30d05d8ccbd0391bbd554bffdd55722815
piyush-773 <piyushlikeyou@gmail.com>
2025-05-12 15:33:32 +0530

    .

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
         setOutput(""); // Clear previous output
 
         try {
-            const response = await fetch("http://localhost:5000/compile", {
+            const response = await fetch(import.meta.env.VITE_BASE_URL, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ code, languageId, input: userInput }),
